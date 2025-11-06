1ae33effd1cd19587cb9bfe42ecd9b3d5e87da57
lehaiduong2303-afk <lehaiduong2303@gmail.com>
2025-11-06 09:48:07 +0700

    Add files via upload

---
+++ Snake-master/Snake2021/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "main.hpp"
+#include "function.hpp"
+#include "console.hpp"
+
+//Trang, Vi Tri, Tong muc
+short sPages, sSelectLocation, sTotalCatalog;
+
+int main()
+{
+	try
+	{
+		ConsoleResize(127, 30);
+		DeleteMenu(GetSystemMenu(GetConsoleWindow(), FALSE), SC_MAXIMIZE, MF_BYCOMMAND);
+		SetConsoleTitle(TEXT("Snake Game - Nhom 11"));
+		ShowCur(false);
+		sPages = 1;
+		EventProcessing();
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "Error.\n";
+	}
+	return 0;
+}
